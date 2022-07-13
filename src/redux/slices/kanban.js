23df07353c56ce8458b7047b5ec08f01bbd14ff7
vancattuyen92 <vancattuyen92@gmail.com>
2022-07-13 00:43:16 -0600

    add user function

--- src/redux/slices/kanban.js
+++ src/redux/slices/kanban.js
@@ -39,14 +39,14 @@ export const kanbanSlices = createSlice({
       console.log('deelete: ', JSON.parse(JSON.stringify(state.board)))
     },
     updateCardInfo: (state, action) => {
-        const { cardId, columnId } = action.payload;
+        
 
     },
     updateColumnInfo: (state, action) => {
 
     },
     viewCard: (state, action) => {
-      const { cardId, columnID } = action.payload;
+     
 
     }
   }
